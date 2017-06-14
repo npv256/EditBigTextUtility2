b24efbee119191e4b9bb7372dafcf4372429dee8
Wayha <Wayha@DESKTOP-JII5VPC>
2017-06-14 14:30:39 +0700

    Добавьте файлы проекта.

---
+++ TextEditTask/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace TextEditTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(1, 128);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Punctuation marks";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(1, 105);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(102, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Amount symbols";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(191)))), ((int)(((byte)(204)))));
+            this.button1.Location = new System.Drawing.Point(1, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "LoadFile";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(112, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(112, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(191)))), ((int)(((byte)(204)))));
+            this.button2.Location = new System.Drawing.Point(142, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.CreatePrompt = true;
+            this.saveFileDialog1.DefaultExt = "txt";
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(239)))), ((int)(((byte)(242)))));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(1, 151);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(275, 276);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "File";
+            this.columnHeader1.Width = 160;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Progress";
+            this.columnHeader2.Width = 113;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 62);
+            this.panel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(106)))), ((int)(((byte)(109)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(279, 430);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "TextEdit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ TextEditTask/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEditTask
+{
+    public partial class Form1 : Form
+    {
+        private Encoding encoding { get; set; }
+
+        private string fileLoadName { get; set; }
+
+        private string fileSaveName { get; set; }
+
+        private string DirectorySaveName { get; set; }
+
+        private char[] punctMarks { get; set; }
+
+        private int amountSymbols { get; set; }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true) textBox1.Enabled = true;
+            else
+            {
+                textBox1.Text = null;
+                textBox1.Enabled = false;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true) textBox2.Enabled = true;
+            else
+            {
+                textBox2.Text = null;
+                textBox2.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            this.fileLoadName = openFileDialog1.FileName;
+            this.encoding = Encoding.GetEncoding("Windows-1251");
+
+            MessageBox.Show("File selected");
+        }
+
+
+        private bool checkCorrect()
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(textBox1.Text)) this.amountSymbols = Convert.ToInt16(textBox1.Text);
+                else this.amountSymbols = -1;
+                if (!String.IsNullOrEmpty(textBox2.Text))
+                    punctMarks = textBox2.Text.ToCharArray();
+                if (String.IsNullOrEmpty(textBox1.Text) && String.IsNullOrEmpty(textBox2.Text)) return false;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void editText()
+        {
+            string fileEditText = "";
+
+            string fileLoadName = this.fileLoadName;
+
+            char[] punctMarks = this.punctMarks;
+
+            int amountSymbols = this.amountSymbols;
+
+           
+
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter(DirectorySaveName);
+
+             if (punctMarks!=null && punctMarks.Length!=0 && amountSymbols > 0) // sort punctMarks && amountSymbol
+             {
+                    foreach (var line in File.ReadLines(fileLoadName,this.encoding))
+                    {
+                        string[] lineWords = line.Split(' ');
+                        foreach (var word in lineWords)
+                        {
+                            string editWord = word;
+                            editWord = word.Trim(punctMarks);
+                            if (editWord.Length > amountSymbols) fileEditText += editWord + ' ';
+                        }
+                    fileEditText =  fileEditText.Remove(fileEditText.LastIndexOf(' '), 1);
+                    file.WriteLine(fileEditText, false, Encoding.GetEncoding("Windows-1251"));
+                    fileEditText = "";
+                    }
+              }
+
+             else if (amountSymbols < 1)   // sort of puctMarks
+             {
+                    foreach (string line in File.ReadLines(fileLoadName, this.encoding))
+                    {
+                    string[] lineWords = line.Split(' ');
+                    foreach (var word in lineWords)
+                    {
+                        string editWord = word;
+                        editWord = word.Trim(punctMarks);
+                        fileEditText += editWord + ' ';
+                    }
+                    fileEditText = fileEditText.Remove(fileEditText.LastIndexOf(' '), 1);
+                    file.WriteLine(fileEditText, DirectorySaveName, this.encoding);
+                    fileEditText = "";
+                }
+             }
+
+            else if (punctMarks==null || punctMarks.Length==0) // sort of amountSymbol
+            {
+                foreach (var line in File.ReadLines(fileLoadName, this.encoding))
+                {
+                    string[] lineWords = line.Split(' ');
+                    foreach (var word in lineWords)
+                    {
+                        string editWord = word;
+                        if (editWord.Length > amountSymbols) fileEditText += editWord + ' ';
+                    }
+                    fileEditText = fileEditText.Remove(fileEditText.LastIndexOf(' '), 1);
+                    file.WriteLine(fileEditText, false, Encoding.GetEncoding("Windows-1251"));
+                    fileEditText = "";
+                }
+            }
+            file.Close(); //Encoding UTF-8
+}
+        public delegate void InvokeDelegate();
+
+        public void InvokeMethod()     
+        {
+            var indexItem = listView1.FindItemWithText(fileSaveName).Index; 
+            listView1.Items[indexItem].SubItems[1].Text = "Complete";                   
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            this.DirectorySaveName = saveFileDialog1.FileName;
+            this.fileSaveName = saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.LastIndexOf('\\'));
+
+            if (this.fileLoadName != null && fileSaveName!= null && checkCorrect())
+            {
+                ListViewItem item = new ListViewItem(fileSaveName);
+                item.SubItems.Add("Progress");
+                listView1.Items.Add(item);
+                ThreadPool.QueueUserWorkItem(new WaitCallback((s) =>
+                {
+                          editText();
+                          listView1.BeginInvoke(new InvokeDelegate(InvokeMethod));
+                }));
+            }
+            else
+            {
+                MessageBox.Show("Input data is not correct");
+            }
+        }
+
+
+    }
+}
